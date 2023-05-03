9bfabd62b7effbcc8247624fd393a8ab24872aae
Novian Rachman Iskandar <novian19@upi.edu>
2023-05-03 11:09:33 +0700

    fix binding on age

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -17,7 +17,7 @@ type UserController struct {
 type UserDto struct {
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Username string `json:"username" form:"username" binding:"required"`
-	Password string `json:"password" form:"password" binding:"required,min=8"`
+	Password string `json:"password" form:"password" binding:"required"`
 	Age      uint   `josn:"age" form:"age"`
 }
 
